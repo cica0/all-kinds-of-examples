1317531071d9193155577595a1c0ff618a015a91
cica0 <liuchang358@163.com>
2017-11-02 11:00:11 +0800

    change lock

--- lock/lock.cpp
+++ lock/lock.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
+#include <boost/thread.hpp>
 #include <pthread.h>
-#define NUM_THREADS 3
+using namespace std;
 
-using std::cout;
-
-//mutex m;
 int sh = 8;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-void *f(void *argv)
-{
-//    unique_lock<mutex> lck(m);
-    cout << "enter into f";
+void my_mutex() {
+    pthread_mutex_lock(&mutex);
     sh += 10; 
-    cout << "sh = " << sh;
-    return (void *)NULL;
+    cout << "sh = " << sh << std::endl;
+    pthread_mutex_unlock(&mutex);
 }
 
+void my_cond() {
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&cond, &mutex);
+    sh += 10; 
+    cout << "sh = " << sh << std::endl;
+    pthread_mutex_unlock(&mutex);
+}
+
+void test_mutex() {
+    boost::thread thread1(my_mutex);
+    boost::thread thread2(my_mutex);
+    boost::thread thread3(my_mutex);
+    thread1.join();
+    thread2.join();
+    thread3.join();
+}
+
+void test_cond() {
+    boost::thread thread1(my_cond);
+    boost::thread thread2(my_cond);
+    boost::thread thread3(my_cond);
+    for(int i =0;i<3;++i) {
+        sleep(3);           //这里如果不加sleep会造成死锁
+        pthread_cond_signal(&cond);
+    }
+    thread1.join();
+    thread2.join();
+    thread3.join();
+}
 
 int main()
 {
-    pthread_t tid[NUM_THREADS];
-    cout << "enter into main" << std::endl;
-    int ret = 0;
-    for(int i = 0; i<NUM_THREADS; ++i)
-    {
-        cout << "i is " << i << std::endl;
-        ret = pthread_create(&tid[i], NULL, f, NULL);
-        cout << "ret is " << ret << std::endl;
-        if(ret != 0)
-            cout << "pthread_create error";
-    }
+//    test_mutex();
+    test_cond();
     return 0;
 }
