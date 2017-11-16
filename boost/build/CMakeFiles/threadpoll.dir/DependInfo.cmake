cf338274c30741589f49ffd09aa2f59a3948f394
cica0 <liuchang358@163.com>
2017-11-16 15:19:23 +0800

    add threadpool of boost

---
+++ boost/build/CMakeFiles/threadpoll.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chang/study/all-kinds-of-examples/boost/threadpool.cpp" "/home/chang/study/all-kinds-of-examples/boost/build/CMakeFiles/threadpoll.dir/threadpool.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "NDEBUG"
+  "THREADED"
+  "VERSION_ID=\"threadpoll-0.0.1.1\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/chang/work/common/third/glog/include"
+  "/home/chang/work/common/third/gflags/include"
+  "/home/chang/work/common/third/gtest/include"
+  "/home/chang/work/common/third/curl/include"
+  "/home/chang/work/common/third/boost/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ boost/build/CMakeFiles/threadpoll.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/threadpoll.dir/threadpool.cpp.o"
+  "threadpoll.pdb"
+  "threadpoll"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/threadpoll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
