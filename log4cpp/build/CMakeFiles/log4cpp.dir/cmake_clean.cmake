c1b49dc669a66716db7e1198448a0bac234a231c
cica0 <liuchang358@163.com>
2017-12-20 17:37:12 +0800

    have a test of log4cpp

---
+++ log4cpp/build/CMakeFiles/log4cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/log4cpp.dir/main.cpp.o"
+  "log4cpp.pdb"
+  "log4cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/log4cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
